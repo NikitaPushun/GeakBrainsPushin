5f620a9ed1ba992811b3d9dbc2809c4e81e18ad2
Nikita <pushingames@gmail.com>
2020-07-09 19:23:21 +0300

    Звук выстрела

--- Assets/Scripts/MyScripts/ShootingCharacter.cs
+++ Assets/Scripts/MyScripts/ShootingCharacter.cs
@@ -6,13 +6,16 @@ public class ShootingCharacter : MonoBehaviour
 {
     [SerializeField] private GameObject _bullet;
     [SerializeField] private Transform _spawnPointBullet;
+    [SerializeField] private AudioSource _fireSound;
     
     
     void Update()
     {
         if (Input.GetButtonDown("Fire2"))
         {
-            Shoot();            
+            Shoot();
+            _fireSound.Play();
+            //GetComponent<AudioSource>().PlayOneShot(_fireSound);
         }        
     }
        
