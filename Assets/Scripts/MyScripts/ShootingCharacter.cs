044f19677274a77055b5fb1d07743d317e7291f3
Nikita <pushingames@gmail.com>
2020-07-09 17:46:19 +0300

    New project

---
+++ Assets/Scripts/MyScripts/ShootingCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingCharacter : MonoBehaviour
+{
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private Transform _spawnPointBullet;
+    
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Shoot();            
+        }        
+    }
+       
+
+    private void Shoot()
+    {
+        Instantiate(_bullet, _spawnPointBullet.position, _spawnPointBullet.rotation);
+
+    }
+}
